198ca7f8384173351da4c19b21117aa602c158ad
Anton Karl Ingason <anton.karl.ingason@gmail.com>
2010-07-01 18:44:20 +0000

    added release script

---
+++ release.sh
@@ -0,0 +1,25 @@
+CORPUSNAME="icepahc"
+VERSION="0.1"
+DIRNAME=$CORPUSNAME"-v"$VERSION"/"
+TEMPFILE="temp.psd"
+rm -rf $DIRNAME
+mkdir $DIRNAME
+cp lgpl.txt $DIRNAME"lgpl.txt"
+cp gpl.txt $DIRNAME"gpl.txt"
+cp README $DIRNAME"README"
+
+mkdir $DIRNAME"psd"
+mkdir $DIRNAME"info"
+
+cp finished/*.psd $DIRNAME"psd"
+cp info/*.info $DIRNAME"info"
+
+for i in $(ls icepahc-v0.1/psd/*.psd);
+do
+echo "/*" >> $TEMPFILE
+cat README >> $TEMPFILE
+echo "*/" >> $TEMPFILE
+cat $i >> $TEMPFILE
+mv $TEMPFILE $i
+done
+exit

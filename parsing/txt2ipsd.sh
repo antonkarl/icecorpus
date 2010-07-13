a98168506d93d46663bc477baff2fa9cb33831a5
Anton Karl Ingason <anton.karl.ingason@gmail.com>
2010-07-13 17:07:29 +0000

    fixed script

---
+++ parsing/txt2ipsd.sh
@@ -0,0 +1,28 @@
+echo "Encoding special markup"
+python3 ./scripts/encodemarkup.py $1.txt $1.enc
+
+echo "Tagging using IceTagger"
+cat $1.enc | java -classpath "../IceNLP/IceNLPCore.jar" is.iclt.icenlp.runner.RunIceTagger > $1.tagged
+
+echo "Lemmatizing using Lemmald"
+cat $1.enc | java -classpath "../IceNLP/IceNLPCore.jar" is.iclt.icenlp.runner.RunIceTagger -of 1 -lem > $1.lemmatized
+
+# python3 ./scripts/joinlemma.py $1.tagged $1.lemmatized
+
+echo "Parsing using IceParser"
+cat $1.tagged | java -classpath "../IceNLP/IceNLPCore.jar" is.iclt.icenlp.runner.RunIceParser -f -l > $1.ipsdx
+
+echo "Converting IceParser's ipsd output to labeled bracketing"
+# Assumes .ipsd input and .psd output
+python3 ./scripts/ipsd2psd.py $1
+
+echo "Decoding special markup"
+python ./scripts/decodemarkup.py $1.ipsd $1.ipsd
+
+echo "Removing temporary files"
+rm $1.enc
+rm $1.tagged
+rm $1.lemmatized
+rm $1.ipsdx
+
+echo "Done"

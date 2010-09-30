510e7d163c50a51b716e302d0616e9088167a50c
Einar Freyr Sigurðsson <einar@einar-laptop.(none)>
2010-09-30 12:55:14 +0000

    updated RP check on verbs

--- parsing/scripts/wordlevel-sanity.py
+++ parsing/scripts/wordlevel-sanity.py
@@ -45,7 +45,7 @@ req_lemma.append( ("PRO-D","því","það","lemma for því should be það") )
 
 # In this section the program complains if all the fields are matched
 bad=[]
-bad.append( ("VB\S*",anything,"(út|inn)([a-zA-ZþæðöÞÆÐÖáéýúíóÁÉÝÚÍÓ]+)a","should a particle be split off here?") )
+bad.append( ("VB\S*",anything,"(út|inn|á|af|að|fram|frá|fyrir|hjá|í|niður|með|of|til|um|upp|úr|við|yfir)([a-zA-ZþæðöÞÆÐÖáéýúíóÁÉÝÚÍÓ]+)a","should a particle be split off here?") )
 
 ##################### END OF PATTERNS THAT SHOULD BE EDITED ############################
 
